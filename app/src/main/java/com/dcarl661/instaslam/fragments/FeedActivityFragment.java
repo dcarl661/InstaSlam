09d0272ca70ad49608a06b82441505b7b382f084
developer8\david carlson_2 <dcarl661@gmail.com>
2016-12-12 15:50:33 -0800

    initial

---
+++ app/src/main/java/com/dcarl661/instaslam/fragments/FeedActivityFragment.java
@@ -0,0 +1,43 @@
+package com.dcarl661.instaslam.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dcarl661.instaslam.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FeedActivityFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FeedActivityFragment extends Fragment
+{
+
+    public FeedActivityFragment() {
+        // Required empty public constructor
+    }
+
+    public static FeedActivityFragment newInstance()
+    {
+        FeedActivityFragment fragment = new FeedActivityFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed_activity, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/dcarl661/instaslam/fragments/HomeFragment.java
@@ -0,0 +1,42 @@
+package com.dcarl661.instaslam.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dcarl661.instaslam.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment
+{
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
